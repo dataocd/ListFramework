3f56fdf455abcc58722a1c775bb4b0263db8ca0f
infolock <jhibbard@gmail.com>
2011-05-30 13:38:19 -0400

    Added proper tab to spaces of 4 to code

--- src/AutoLoader.php
+++ src/AutoLoader.php
@@ -8,24 +8,24 @@ class AutoLoader {
      * Updated this property to be static for singleton storage.
      */
     protected static $instance;
-    
+
     protected $namespaces = array();
-    
+
     public static function getInstance() {
         if (null == self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
     }
-    
+
     protected function __construct() {
         $this->registerNamespace('Lists', dirname(__DIR__));
     }
-    
+
     public function registerNamespace($namespace, $dir) {
         $this->namespaces[$namespace] = $dir;
     }
-    
+
     public function autoload($class) {
         if($this->loadClass($class)) {
             return $class;
@@ -40,18 +40,18 @@ class AutoLoader {
      * @param string $class
      */
     public function loadClass($class) {
-      $namespace = strstr("\\", $class, true);
-      $this->classnameToFilename(str_replace($namespace, "", $class), $this->namespaces[$namespace]);
+        $namespace = strstr("\\", $class, true);
+        $this->classnameToFilename(str_replace($namespace, "", $class), $this->namespaces[$namespace]);
     }
-    
+
     public function register() {
         spl_autoload_register(array($this, 'autoload'));
     }
-    
+
     public function classnameToFilename($class, $dir) {
         return $dir . str_replace(self::NAMESPACE_SEPARATOR, DIRECTORY_SEPARATOR, $class) . '.php';
     }
-    
+
     public function fileExists($file) {
         return stream_resolve_include_path($filename);
     }
