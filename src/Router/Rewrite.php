92dd9d81c85cc1a26c0e4426f7cf529cf6e91796
JamesWPhillips <james@jameswphillips.com>
2011-05-31 01:00:10 -0400

    Removed debug echo.

--- src/Router/Rewrite.php
+++ src/Router/Rewrite.php
@@ -6,7 +6,6 @@ class Rewrite implements IRouter {
     public function route(\Lists\Request\Request $request) {
         $FC = \Lists\FrontController::getInstance();
         $uri = str_replace($FC->base_uri,'',$request->URLParts['path']);  
-        echo $uri;
         $paths = explode("/", $uri);
         $request->package = $paths[1];
         $request->packageController = $paths[2];
