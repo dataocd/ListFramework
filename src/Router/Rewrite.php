3c2c197b13959960aef93d8c8956eeccaba13c03
JamesWPhillips <james@jameswphillips.com>
2011-06-02 12:52:29 -0400

    Updated to demo some REST tests

--- src/Router/Rewrite.php
+++ src/Router/Rewrite.php
@@ -1,11 +1,12 @@
 <?php
 namespace Lists\Router;
+use \Lists\FrontController as FC;
 class Rewrite implements IRouter {
     public function __construct() {}
 
     public function route(\Lists\Request\Request $request) {
-        $FC = \Lists\FrontController::getInstance();
-        $uri = str_replace($FC->base_uri,'',$request->URLParts['path']);  
+        $FC = FC::getInstance();
+        $uri = $request->get('PATH_INFO');  
         $paths = explode("/", $uri);
         $request->package = $paths[1];
         $request->packageController = $paths[2];
