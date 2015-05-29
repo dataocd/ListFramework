d0d62f0c912da39701cb3677db076af9a44ce335
AVBC <avbc@users.noreply.github.com>
2015-05-29 11:35:25 -0400

    Update Http.php

--- src/Request/Http.php
+++ src/Request/Http.php
@@ -1,91 +1,67 @@
 <?php
 /**
- * Controller\Request.php
- * The request class is the base class for all types of requests.
- *
- * PHP Version 5.2.0
- * @category  Requests
- * @package   Controller
- * @author    James Phillips <james@dataocd.com>
- * @copyright 2011 DataOCD. All rights reserved.
- * @license   TBD
- * @version   SVN: $Id$
- * @link      http://www.dataocd.com/
+ * @author Jonathon Hibbard
+ * Based on https://github.com/dataocd/ListFramework/blob/master/src/Request/Http.php
  */
+namespace AVBC;
 
-/**
- * @namespace
- */
-namespace Lists\Request;
+class Request {
+    private $variables_order  = array( 'E', 'G', 'P', 'C', 'S' );
 
-/**
- * @category  Requests
- * @package   Controller
- */
- class Http extends Request {
-    private $validMethodTypes = array('DELETE','GET','HEAD','OPTIONS','POST','PUT');
-    private $isHttps = false;
-    private $requestURL;
+    private static $instance = null;
 
-    private $package;
-    private $packageController;
-    private $packageCommand;
+    // Prevent Clone... Singleton...
+    private function __clone() {}
 
-    private $URLParts;
-    private $type;
-    
-    private $basePath;
-    private $baseURL;
-    private $attributes;
-    
-    public function __construct($url = NULL) {
-        //Need to grab the url here
-        //  need to load the put/post and querystrings into the attributes array
-        //  need to set the type
-        
-        if (NULL == $url) {
-            $url = $this->get('REQUEST_URI');
+    private function __construct( $variables_order ) {
+        if( !is_string( $variables_order ) || empty( $variables_order ) ) {
+            $variables_order = strtoupper( ini_get( 'variables_order' ) );
         }
-        $this->URLParts = parse_url($url);
-    }
-    public function getRequestURL() {
-        return $this->requestURL;
+
+        $this->variables_order = str_split( $variables_order );
     }
 
-    public function __get($key) {
-        return $this->$key;
+    private function E() {
+        return $_ENV;
     }
 
-    public function __set($key, $value) {
-        $this->$key = $value;
+    private function G() {
+        return $_GET;
     }
 
-    /**
-     * @author James Phillips
-     * Universal get function. This way, noone else outside of here ever,
-     * need mess with the ugly server globals stuff.
-     * 
-     * @note: Be careful when using this method if you expect to find the 
-     * same value in more than one global store.
-     */
-    public function get($key) {
-        switch (true) {
-            case isset($this->attributes[$key]):
-                return $this->attributes[$key];
-            case isset($_GET[$key]):
-                return $_GET[$key];
-            case isset($_POST[$key]):
-                return $_POST[$key];
-            case isset($_COOKIE[$key]):
-                return $_COOKIE[$key];
-            case ($key == 'REQUEST_URL'):
-                return $this->getRequestUri();
-            case isset($_SERVER[$key]):
-                return $_SERVER[$key];
-            case isset($_ENV[$key]):
-                return $_ENV[$key];
-            default:
-                return null;
+    private function P() {
+        return $_POST;
+    }
+
+    private function C() {
+        return $_COOKIE;
+    }
+
+    private function S() {
+        return $_SERVER;
+    }
+
+    public static function getInstance( $variables_order = 'EGPCS' ) {
+        if( !isset( self::$instance ) ) {
+            self::$instance = new Request( $variables_order );
         }
+
+        return self::$instance;
+    }
+
+    public function valueForKey( $key ) {
+        if( !is_string( $key ) || empty( $key ) ) {
+            return null;
+        }
+
+        foreach( $this->variables_order as $prop_type ) {
+            $_prop = $this->$prop_type();
+
+            if( isset( $_prop[$key] ) ) {
+                return $_prop[$key];
+            }
+        }
+
+        return null;
     }
 }
